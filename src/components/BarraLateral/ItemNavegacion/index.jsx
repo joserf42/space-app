d20d8409c1fa166ceedc88481798bbbb2270e642
joserf42 <107516670+joserf42@users.noreply.github.com>
2024-06-18 16:42:42 -0500

    proyecto spaceApp

--- src/components/BarraLateral/ItemNavegacion/index.jsx
+++ src/components/BarraLateral/ItemNavegacion/index.jsx
@@ -8,6 +8,7 @@ const ItemListaEstilizado = styled.li`
     align-items: center;
     gap: 16px;
     color: ${props => props.$activo ? "#7B78E5" : "#D9D9D9"};
+    font-family: ${props => props.$activo ? "GandhiSansBold" : "GandhiSansRegular"};
 `
 
 const ItemNavegacion = ({children, iconoActivo, iconoInactivo, activo=false}) => {
