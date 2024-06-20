491df6399f479e614bc16b9b287967a570a95a9b
joserf42 <107516670+joserf42@users.noreply.github.com>
2024-06-19 20:02:03 -0500

    trabajando en el banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components"
 import GlobalStyles from "./components/GlobalStyles"
 import Cabecera from "./components/Cabecera"
 import BarraLateral from "./components/BarraLateral"
+import Banner from "./components/Banner"
+import banner from "./assets/banner.png"
 
 const FondoGradiente = styled.div`
 background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -10,15 +12,29 @@ min-height: 100vh;
 
 `
 
+const AppContainer = styled.div`
+  width: 1280px;
+  max-width: 100%;
+  margin: 0 auto;
+`
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`
+
 function App() {
   
 
   return (
     <>
      <FondoGradiente>
-     <GlobalStyles />
-     <Cabecera />
-     <BarraLateral />
+      <GlobalStyles />
+      <Cabecera />
+      <AppContainer>
+        <MainContainer> </MainContainer>
+      <BarraLateral />
+      <Banner  texto = "La galería más completa de fotos del Espacio" backgroundImage={banner}/>
+      </AppContainer>
      </FondoGradiente> 
     </>
   )
--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,3 +1,5 @@
+import { styled } from "styled-components";
+
 const FigureEstilizada = styled.figure`
     background-image: ${props => `url(${props.$backgroundImage})`};
     flex-grow: 1;
